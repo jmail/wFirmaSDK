62e794975f702db4804b2736a92e2fb496383cd7
Jarek Krzemieński <jarek@webzzmaster.com>
2018-03-18 04:05:25 +0100

    ConfigParser cleanup

--- src/WebzzMaster/WFirmaSdk/Components/ConfigParser.php
+++ src/WebzzMaster/WFirmaSdk/Components/ConfigParser.php
@@ -35,6 +35,96 @@ class ConfigParser
         }
     }
     
+    protected function extractApiUrl(array $config)
+    {
+        if (!isset($config['webzzmaster']['wfirmasdk']['url'])) {
+            throw new Exception(
+                "wFirmaSDK is not properly configured: missing configurations key: url"
+            );
+        }
+        $this->setApiUrl($config['webzzmaster']['wfirmasdk']['url']);
+    }
+    
+    protected function extractAuthentication(array $config)
+    {
+        if (!isset($config['webzzmaster']['wfirmasdk']['authentication'])) {
+            throw new Exception(
+                "wFirmaSDK is not properly configured: missing configurations key: authentication"
+            );
+        }
+        $this->setAuthentication($config['webzzmaster']['wfirmasdk']['authentication']);
+    }
+    
+    protected function extractConsumerKey(array $config)
+    {
+        if (!isset($config['webzzmaster']['wfirmasdk']['consumer_key'])) {
+            throw new Exception(
+                "wFirmaSDK is not properly configured: missing configurations key: consumer_key"
+            );
+        }
+        $this->setConsumerKey($config['webzzmaster']['wfirmasdk']['consumer_key']);
+    }
+    
+    protected function extractConsumerSecret(array $config)
+    {
+        if (!isset($config['webzzmaster']['wfirmasdk']['consumer_secret'])) {
+            throw new Exception(
+                "wFirmaSDK is not properly configured: missing configurations key: consumer_secret"
+            );
+        }
+        $this->setConsumerSecret($config['webzzmaster']['wfirmasdk']['consumer_secret']);
+    }
+    
+    protected function extractPassword(array $config)
+    {
+        if (!isset($config['webzzmaster']['wfirmasdk']['password'])) {
+            throw new Exception(
+                "wFirmaSDK is not properly configured: missing configurations key: password"
+            );
+        }
+        $this->setPassword($config['webzzmaster']['wfirmasdk']['password']);
+    }
+    
+    protected function extractUsername(array $config)
+    {
+        if (!isset($config['webzzmaster']['wfirmasdk']['login'])) {
+            throw new Exception(
+                "wFirmaSDK is not properly configured: missing configurations key: login"
+            );
+        }
+        $this->setUsername($config['webzzmaster']['wfirmasdk']['login']);
+    }
+    
+    public function getApiUrl()
+    {
+        return $this->apiUrl;
+    }
+
+    public function getAuthentication()
+    {
+        return $this->authentication;
+    }
+    
+    public function getConsumerKey()
+    {
+        return $this->consumerKey;
+    }
+
+    public function getConsumerSecret()
+    {
+        return $this->consumerSecret;
+    }
+    
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    
     public function parse(array $config)
     {
         if (!isset($config['webzzmaster']['wfirmasdk'])) {
@@ -43,90 +133,26 @@ class ConfigParser
             );
         }
         
-        if (!isset($config['webzzmaster']['wfirmasdk']['url'])) {
-            throw new Exception(
-                "wFirmaSDK is not properly configured: missing configurations key: url"
-            );
-        }        
-        $this->setApiUrl($config['webzzmaster']['wfirmasdk']['url']);
-        
-        if (!isset($config['webzzmaster']['wfirmasdk']['authentication'])) {
-            throw new Exception(
-                "wFirmaSDK is not properly configured: missing configurations key: authentication"
-            );
-        }        
-        $this->setAuthentication($config['webzzmaster']['wfirmasdk']['authentication']);
+        $this->extractApiUrl($config);        
+        $this->extractAuthentication($config);
         
         if($config['webzzmaster']['wfirmasdk']['authentication'] === 'basic'){
-            if (!isset($config['webzzmaster']['wfirmasdk']['login'])) {
-                throw new Exception(
-                    "wFirmaSDK is not properly configured: missing configurations key: login"
-                );
-            }
-            
-            if (!isset($config['webzzmaster']['wfirmasdk']['password'])) {
-                throw new Exception(
-                    "wFirmaSDK is not properly configured: missing configurations key: password"
-                );
-            }
-            $this->setUsername($config['webzzmaster']['wfirmasdk']['login']);
-            $this->setPassword($config['webzzmaster']['wfirmasdk']['password']);
+            $this->extractUsername($config);
+            $this->extractPassword($config);
         }
         
         if($config['webzzmaster']['wfirmasdk']['authentication'] === 'oauth'){
-            if (!isset($config['webzzmaster']['wfirmasdk']['consumer_key'])) {
-                throw new Exception(
-                    "wFirmaSDK is not properly configured: missing configurations key: consumer_key"
-                );
-            }
             
-            if (!isset($config['webzzmaster']['wfirmasdk']['consumer_secret'])) {
-                throw new Exception(
-                    "wFirmaSDK is not properly configured: missing configurations key: consumer_secret"
-                );
-            }
         }
         
     }
-
+    
     public function setApiUrl(string $url)
     {
         $violations = $this->validator->validate(
             $url,
             [
                 new Url(),
-                new NotBlank(),
-            ]
-        );
-
-        if (0 !== count($violations)) {
-            throw new Exception(
-                "wFirmaSDK is not properly configured - ".$this->generateViolationsMessage($violations)
-            );
-        }
-    }
-
-    public function setUsername(string $username)
-    {
-        $violations = $this->validator->validate(
-            $username,
-            [
-                new NotBlank(),
-            ]
-        );
-
-        if (0 !== count($violations)) {
-            throw new Exception(
-                "wFirmaSDK is not properly configured - ".$this->generateViolationsMessage($violations)
-            );
-        }
-    }
-
-    public function setPassword(string $password)
-    {
-        $violations = $this->validator->validate(
-            $password,
-            [
                 new NotBlank(),
             ]
         );
@@ -176,7 +202,7 @@ class ConfigParser
             );
         }
     }
-
+    
     public function setConsumerSecret(string $consumerSecret)
     {
         $violations = $this->validator->validate(
@@ -192,34 +218,36 @@ class ConfigParser
             );
         }
     }
-
-    public function getApiUrl()
+    
+    public function setPassword(string $password)
     {
-        return $this->apiUrl;
+        $violations = $this->validator->validate(
+            $password,
+            [
+                new NotBlank(),
+            ]
+        );
+
+        if (0 !== count($violations)) {
+            throw new Exception(
+                "wFirmaSDK is not properly configured - ".$this->generateViolationsMessage($violations)
+            );
+        }
     }
-
-    public function getUsername()
+    
+    public function setUsername(string $username)
     {
-        return $this->username;
-    }
+        $violations = $this->validator->validate(
+            $username,
+            [
+                new NotBlank(),
+            ]
+        );
 
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function getAuthentication()
-    {
-        return $this->authentication;
-    }
-
-    public function getConsumerKey()
-    {
-        return $this->consumerKey;
-    }
-
-    public function getConsumerSecret()
-    {
-        return $this->consumerSecret;
+        if (0 !== count($violations)) {
+            throw new Exception(
+                "wFirmaSDK is not properly configured - ".$this->generateViolationsMessage($violations)
+            );
+        }
     }
 }
